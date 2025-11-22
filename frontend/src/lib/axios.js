38fe7295f82ad876783f00eb5bf7a25e8a540cf8
monsemontalvo <monzzeerrat@hotmail.com>
2025-11-21 18:58:38 -0600

    Ajustes para hosteo y cambio de titulo

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5003/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5003/api": "/api",
     withCredentials: true, //Para enviar cookies en solicitudes CORS
 });
