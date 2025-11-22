8f3e5d27c9c9a4bae0951983b644f9b1e45c9657
monsemontalvo <monzzeerrat@hotmail.com>
2025-11-21 19:30:44 -0600

    Cambio para lectura de render

--- backend/src/index.js
+++ backend/src/index.js
@@ -42,7 +42,7 @@ app.use("/api", emailRoutes);             // <--- Para enviar correos
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get(/^(.*)$/, (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
